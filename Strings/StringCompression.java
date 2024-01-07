54578ce2a786667baabf17e663ffe2fc117d176b
Harsh Jain <hvj.techture@gmail.com>
2024-01-07 17:37:22 +0530

    added few questions

---
+++ Strings/StringCompression.java
@@ -0,0 +1,50 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class StringCompression {
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    String str = scn.next();
+    System.out.println(compression(str));
+    System.out.println(compression2(str));
+    scn.close();
+  }
+
+  private static String compression(String str) {
+    String s = Character.toString(str.charAt(0));
+    for(int i =1; i < str.length();i++) {
+      char curr = str.charAt(i);
+      char prev = str.charAt(i-1);
+      if(curr != prev) {
+        s += curr;
+      }
+    }
+    return s;
+  }
+
+  private static String compression2(String str) {
+    String s = Character.toString(str.charAt(0));
+    int count = 1;
+
+    for(int i=1; i < str.length();i++) {
+      char curr = str.charAt(i);
+      char prev = str.charAt(i-1);
+      if(curr == prev) {
+        count ++;
+      }
+      else {
+        if(count > 1) {
+          s += count;
+          count = 1;
+        }
+        s += curr;
+      }
+    }
+    if(count > 1) {
+      s += count;
+      count = 1;
+    }
+    return s;
+  }
+}
---
+++ Strings/ToggleCase.java
@@ -0,0 +1,29 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class ToggleCase {
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    String s = scn.next();
+    System.out.println(toggleCase(s));
+    scn.close();
+  }
+
+  private static String toggleCase(String str) {
+    StringBuilder sb = new StringBuilder(str);
+    for(int i = 0; i < sb.length();i++) {
+      char ch = sb.charAt(i);
+
+      if(ch >= 'a' && ch <= 'z') {
+        char uch = (char)('A' + ch - 'a');
+        sb.setCharAt(i, uch);
+      }
+      else if(ch >= 'A' && ch <= 'Z') {
+        char lch = (char)('a' + ch - 'A');
+        sb.setCharAt(i, lch);
+      }
+    }
+    return sb.toString();
+  }
+}
